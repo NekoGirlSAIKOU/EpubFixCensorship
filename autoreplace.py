9e433b4d7d183d99804707c686f28a69913e77b5
NekoGirlSAIKOU <nekogirlsaikou@outlook.com>
2021-05-03 19:17:41 +0800

    Initial commit

---
+++ autoreplace.py
@@ -0,0 +1,41 @@
+from typing import List, Tuple
+
+from lxml import etree
+
+try:
+    import regex as re
+except ImportError:
+    import re
+
+
+class AutoReplace:
+    def __init__(self, *, rules: List[dict] = [], ):
+        self.rules = rules
+
+    def replace_text(self, text) -> List[Tuple[str, str]]:
+        r = [('origin', text)]
+        for replace_rule in self.rules:
+            replace_rule['name'] = replace_rule.get('name', replace_rule.get('replaceSummary', ''))
+            replace_rule['pattern'] = replace_rule.get('pattern', replace_rule.get('regex'))
+            replace_rule['replacement'] = replace_rule.get('replacement', '')
+            replace_rule['isEnabled'] = replace_rule.get('isEnabled', replace_rule.get('enable', True))
+            if replace_rule['name'] == '' or replace_rule['name'] is None:
+                replace_rule['name'] = f'{replace_rule["pattern"]} to {replace_rule["replacement"]}'
+
+            if replace_rule['isEnabled'] is False:
+                continue
+
+            if replace_rule.get('isRegex', False):
+                try:
+                    r = re.findall(replace_rule['pattern'], text)
+                    new_text = re.sub(replace_rule['pattern'], replace_rule['replacement'], text, count=0, flags=0)
+                    if new_text != text:
+                        r.append((replace_rule['name'], new_text))
+                except Exception as e:
+                    replace_rule['isEnabled'] = False
+                    continue
+            else:
+                new_text = text.replace(replace_rule['pattern'], replace_rule['replacement'])
+                if new_text != text:
+                    r.append((replace_rule['name'], new_text))
+        return r
---
+++ main.py
@@ -0,0 +1,238 @@
+import json
+import sys
+from typing import Optional, List, Tuple
+
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QFileDialog, QMessageBox, QProgressDialog, \
+    QListWidgetItem
+from ebooklib import epub
+import ebooklib
+from lxml import etree
+from MainWindow import *
+from autoreplace import AutoReplace
+
+VERSION = (0, 1, 0)
+VERSION_NAME = '.'.join(str(i) for i in VERSION)
+app: QApplication = None
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.book: Optional[epub.EpubBook] = None
+        self.file_name: Optional[str] = None
+        self.chapters: List[epub.EpubHtml] = []
+        self.current_chapter_index = 0
+        self.current_chapter: Optional[etree._Element] = None
+        self.chapter_elements: List[etree._Element] = []
+        self.current_element_index = 0
+
+        self.auto_replacements:List[Tuple[str,str]]=[]
+
+        try :
+            with open ('rules.json') as f:
+                self.rules = json.load(f)
+        except FileNotFoundError:
+            self.rules = []
+
+        self.replacer = AutoReplace(rules=self.rules)
+
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        self.ui.actionOpen.triggered.connect(self.open_book_dialog_box)
+        self.ui.actionSave.triggered.connect(self.save_book)
+        self.ui.actionSave_as.triggered.connect(self.save_book_dialog_box)
+        self.ui.actionExit.triggered.connect(self.close)
+
+        self.ui.actionAbout.triggered.connect(self.show_about_window)
+        self.ui.actionAbout_Qt.triggered.connect(self.show_about_qt)
+
+        self.ui.next_button.clicked.connect(self.next_element)
+        self.ui.last_button.clicked.connect(self.last_element)
+
+        self.ui.reapply_button.clicked.connect(self.button_reapply_clicked)
+        self.ui.auto_fix_list.itemDoubleClicked.connect(self.listwidget_item_double_clicked)
+
+    def closeEvent(self, QCloseEvent):
+        if self.book is not None:
+            self.ask_will_save()
+        app.exit()
+
+    def open_book_dialog_box(self):
+        if self.book is not None:
+            self.ask_will_save()
+
+        file_name = QFileDialog.getOpenFileName()[0]
+        if file_name != "":
+            self.open_book(file_name)
+
+    def open_book(self, file_name):
+        self.file_name = file_name
+        self.book = epub.read_epub(self.file_name)
+        self.book.set_unique_metadata(None, 'meta', '', {'name': 'EpubFixCensorship version', 'content': VERSION_NAME})
+
+        # clear last book ui
+        self.ui.censored_text.clear()
+        self.ui.fixed_text.clear()
+        self.ui.auto_fix_list.clear()
+
+        # clear last book cache
+        self.chapters.clear()
+        self.current_chapter_index = -1
+        self.current_chapter = None
+        self.chapter_elements.clear()
+        self.current_element_index = 0
+
+        # load new book
+        self.chapters = list(self.book.get_items_of_type(ebooklib.ITEM_DOCUMENT))
+        self.next_chapter()
+
+    def save_book_dialog_box(self):
+        file_name = QFileDialog.getSaveFileName()[0]
+        if file_name != "":
+            self.file_name = file_name
+            self.save_book()
+
+    def save_book(self, file_name: Optional[str] = None):
+        if self.book is None:
+            return
+
+        if file_name is None:
+            if self.file_name is None:
+                return
+            file_name = self.file_name
+
+
+        # Make sure current edit is saved.
+        self.save_current_element()
+        self.save_current_chapter()
+
+        epub.write_epub(file_name, self.book)
+
+    def show_about_window(self):
+        QMessageBox.about(self, "About:", f"EpubFixCensorship\nVersion:{VERSION_NAME}")
+
+    def show_about_qt(self):
+        QMessageBox.aboutQt(self)
+
+    def ask_will_save(self):
+        # Ask the user if he want to save the epub.
+        r = QMessageBox.question(self, "Do you want to save", "Do you want to save current book?")
+        if r == QMessageBox.StandardButton.Yes:
+            self.save_book()
+
+    def last_element(self):
+        self.save_current_element()
+        if self.current_element_index <= 0:
+            # There is no more element
+            # Last chapter
+            self.last_chapter()
+        else:
+            # Next element
+            self.current_element_index -= 1
+            self.show_element()
+
+    def next_element(self):
+        self.save_current_element()
+        if self.current_element_index >= len(self.chapter_elements) - 1:
+            # There is no more element
+            # Next chapter
+            self.next_chapter()
+        else:
+            # Next element
+            self.current_element_index += 1
+            self.show_element()
+
+    def save_current_element(self):
+        try :
+            current_element = self.chapter_elements[self.current_element_index]
+            if current_element.get('censored_text') is None:
+                current_element.set('censored_text',current_element.text)
+            current_element.text = self.ui.fixed_text.toPlainText()
+        except IndexError:
+            pass
+
+    def show_element(self,original_text = None):
+        current_element = self.chapter_elements[self.current_element_index]
+        if original_text is None:
+            original_text = current_element.get("censored_text",current_element.text)
+
+        self.ui.auto_fix_list.clear()
+        self.ui.censored_text.setPlainText(original_text)
+
+        self.auto_replacements = self.replacer.replace_text(original_text)
+        try :
+            # index 0 is origin result
+            self.ui.fixed_text.setPlainText(self.auto_replacements[1][1])
+        except IndexError:
+            self.ui.fixed_text.setPlainText(current_element.text)
+        for replacement in self.auto_replacements:
+            self.ui.auto_fix_list.addItem(replacement[0])
+
+    def last_chapter(self):
+        self.save_current_chapter()
+        if self.current_chapter_index <= 0:
+            # There is no more chapter
+            QMessageBox.information(self, "Information", "This is the last chapter")
+            return
+        else:
+            self.current_chapter_index -= 1
+            self.current_chapter: etree._Element = etree.HTML(
+                self.chapters[self.current_chapter_index].content)
+            self.chapter_elements = self.filter_element(self.current_chapter.cssselect('p,div'))
+            if self.chapter_elements == []:
+                return self.last_chapter()
+            self.current_element_index = len(self.chapter_elements)-1
+
+            self.show_element()
+
+    def next_chapter(self):
+        self.save_current_chapter()
+
+        if self.current_chapter_index >= len(self.chapters) - 1:
+            # There is no more chapter
+            QMessageBox.information(self, "Information", "There is no more chapter")
+            return
+        else:
+            self.current_chapter_index += 1
+            self.current_chapter: etree._Element = etree.HTML(
+                self.chapters[self.current_chapter_index].content)
+            self.chapter_elements = self.filter_element(self.current_chapter.cssselect('p,div'))
+            if self.chapter_elements == []:
+                return self.next_chapter()
+            self.current_element_index = 0
+
+            self.show_element()
+
+    def filter_element(self,elements:List[etree._Element])->List[etree._Element]:
+        r:List[etree._Element] = []
+        for element in elements:
+            if '*' not in element.get('censored_text',element.text):
+                continue
+            r.append(element)
+        return r
+
+    def save_current_chapter(self):
+        if self.current_chapter is not None:
+            self.chapters[self.current_chapter_index].content = etree.tounicode(self.current_chapter)
+
+    def button_reapply_clicked(self):
+        self.save_current_element()
+        self.show_element(original_text=self.ui.fixed_text.toPlainText())
+
+    def listwidget_item_double_clicked(self,item:QListWidgetItem):
+        index = self.ui.auto_fix_list.indexFromItem(item)
+        self.ui.fixed_text.setPlainText(self.auto_replacements[index.row()][1])
+
+
+def main() -> int:
+    global app
+    app = QApplication(sys.argv)
+    main_window = MainWindow()
+    main_window.show()
+    return app.exec_()
+
+
+if __name__ == '__main__':
+    exit(main())
