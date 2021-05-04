fc703b962e637326024dd365ce1bb1007b9189a1
NekoGirlSAIKOU <nekogirlsaikou@outlook.com>
2021-05-04 18:58:00 +0800

    Fix version.py

--- version.py
+++ version.py
@@ -1,5 +1,5 @@
 import subprocess
 
-VERSION = subprocess.check_output(['git', 'describe', '--abbrev=0']).decode('ascii').strip()
-VERSION = list(int(i) for i in VERSION.replace('v','').split('.'))
-VERSION_NAME = subprocess.check_output(['git', 'describe']).decode('ascii').strip()
+VERSION: str = subprocess.check_output(['git', 'describe', '--abbrev=0']).decode('ascii').strip()
+VERSION: tuple = tuple(int(i) for i in VERSION.replace('v', '').split('.'))
+VERSION_NAME: str = subprocess.check_output(['git', 'describe']).decode('ascii').strip()
