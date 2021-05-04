4e8feaf87ae4b811e5efc8ca6232a3cd43da81dd
NekoGirlSAIKOU <nekogirlsaikou@outlook.com>
2021-05-04 23:26:14 +0800

    Small change for build for windows

--- build_windows.bat
+++ build_windows.bat
@@ -2,7 +2,6 @@ python build.py
 mkdir build_windows
 mkdir temp
 pyinstaller -w -F -n EpubFixCensorship --distpath build_windows --workpath temp build\main.py
-del temp
 copy /Y LICENSE build_windows\LICENSE
 copy /Y README.md build_windows\README.md
 copy /Y rules.json build_windows\rules.json
