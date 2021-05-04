06854dfbbcd9b52f4390908ae2f0216204975d4e
NekoGirlSAIKOU <nekogirlsaikou@outlook.com>
2021-05-04 23:08:36 +0800

    Improve build for windows

--- build_windows.bat
+++ build_windows.bat
@@ -1,5 +1,8 @@
 python build.py
 mkdir build_windows
 mkdir temp
-pyinstaller -w -F -n EpubFixCensorship --distpath build_windows --workpath temp build/main.py
+pyinstaller -w -F -n EpubFixCensorship --distpath build_windows --workpath temp build\main.py
 del temp
+copy /Y LICENSE build_windows\LICENSE
+copy /Y README.md build_windows\README.md
+copy /Y rules.json build_windows\rules.json
