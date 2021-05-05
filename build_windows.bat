c90bdd619bcda2fb15c15c63b2c70f4964605a6f
NekoGirlSAIKOU <nekogirlsaikou@outlook.com>
2021-05-05 14:42:11 +0800

    Change some name

--- build_windows.bat
+++ build_windows.bat
@@ -3,7 +3,7 @@ bash build.sh
 mkdir build_windows
 mkdir build_windows\languages
 mkdir temp
-pyinstaller -w -F -n EpubFixCensorship --distpath build_windows --workpath temp build\main.py
+pyinstaller -w -F -n EpubFixCensorship_gui --distpath build_windows --workpath temp build\EpubFixCensorship_gui.py
 copy /Y LICENSE build_windows\LICENSE
 copy /Y README.md build_windows\README.md
 copy /Y rules.json build_windows\rules.json
