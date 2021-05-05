b320f7be7b03eaf510efdb09186c684f2de4a1ba
NekoGirlSAIKOU <nekogirlsaikou@outlook.com>
2021-05-05 15:01:45 +0800

    Patch for better supporting pyinstaller

--- build_windows.bat
+++ build_windows.bat
@@ -1,8 +1,15 @@
-rem git bash is needed.
+@rem git bash is needed.
+
 bash build.sh
 mkdir build_windows
 mkdir build_windows\languages
 mkdir temp
+
+@rem Patch for pyinstaller
+echo= >> build\config.py
+echo import sys >> build\config.py
+echo PROGRAM_PATH = os.path.split(os.path.realpath(sys.argv[0]))[0] >> build\config.py
+
 pyinstaller -w -F -n EpubFixCensorship_gui --distpath build_windows --workpath temp build\EpubFixCensorship_gui.py
 @rem pyinstaller -w -F -n EpubFixCensorship --distpath build_windows --workpath temp build\EpubFixCensorship.py
 copy /Y LICENSE build_windows\LICENSE
