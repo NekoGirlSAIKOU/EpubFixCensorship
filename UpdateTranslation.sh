ad488aad5469691ce4760b0f6afdf8f0ff314e23
NekoGirlSAIKOU <nekogirlsaikou@outlook.com>
2021-05-05 15:10:20 +0800

    Add UpdateTranslation.sh

---
+++ UpdateTranslation.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env sh
+pylupdate5 *.py -ts languages/zh-CN.ts
--- UpdateUI.sh
+++ UpdateUI.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env sh
-# UI
 pyuic5 MainWindow.ui -o MainWindow.py
 pyuic5 SettingWindow.ui -o SettingWindow.py
 pyuic5 ChapterViewerWindow.ui -o ChapterViewerWindow.py
-
-# Translation
-pylupdate5 *.py -ts languages/zh-CN.ts
