c0c70d16c53b4d8c5d222d348c7f25459303dbb9
NekoGirlSAIKOU <nekogirlsaikou@outlook.com>
2021-05-06 16:59:41 +0800

    Fix a bug

--- build_windows.bat
+++ build_windows.bat
@@ -11,7 +11,7 @@ echo import sys >> build\config.py
 echo PROGRAM_PATH = os.path.split(os.path.realpath(sys.argv[0]))[0] >> build\config.py
 
 pyinstaller -w -F -n EpubFixCensorship_gui --distpath build_windows --workpath temp build\EpubFixCensorship_gui.py
-pyinstaller -w -F -n EpubFixCensorship --distpath build_windows --workpath temp build\EpubFixCensorship.py
+pyinstaller -c -F -n EpubFixCensorship --distpath build_windows --workpath temp build\EpubFixCensorship.py
 copy /Y LICENSE build_windows\LICENSE
 copy /Y README.md build_windows\README.md
 copy /Y rules.json build_windows\rules.json
